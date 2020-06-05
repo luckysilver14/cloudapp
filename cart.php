6ffef39043e7243c663f2f9e9973518f0e9f2b16
luckysilver14 <59545411+luckysilver14@users.noreply.github.com>
2020-06-05 10:51:10 +0700

    source change

--- cart.php
+++ cart.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Pc Accessories Shopping Mall</title>
+<title>PC Accessories Online Store</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="myweb.css">
